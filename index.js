175776fef4396f4b52d5f56970b0dab1aa9e3a04
irfanurislam <mdirfanurislam@gmail.com>
2023-06-09 01:42:54 +0600

    first

---
+++ index.js
@@ -0,0 +1,139 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+console.log(process.env.DB_PASS)
+// const uri = "mongodb+srv://coffeeStore:zuTzHhwlyjH1I2t2@cluster0.psezczp.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.psezczp.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+     const classesCollection = client.db('musicDB').collection('classes')
+   
+    // app.get('/coffee',async(req,res) =>{
+    //   const cursor = coffeeCollection.find()
+    //   const result = await cursor.toArray() 
+    //   res.send(result)
+    // })
+
+    // app.get('/coffee/:id', async(req, res) => {
+    //         const id = req.params.id;
+    //         const query = {_id: new ObjectId(id)}
+    //         const result = await coffeeCollection.findOne(query);
+    //         res.send(result);
+    //     })
+
+     app.get('/myclass',async(req,res) =>{  
+      const cursor = classesCollection.find()
+      const result = await cursor.toArray() 
+      res.send(result)
+    })
+
+
+    app.get('/myclass', async (req, res) => {
+      try {
+        const email = req.query.instructorEmail;
+      console.log(email)
+       if(!email){
+        res.send([])
+       }
+       const query = {email:email}
+        // Assuming you have established a MongoDB connection and have access to the "classes" collection
+        const result = await classesCollection.find(query).toArray();
+    
+        res.json(result);
+      } catch (error) {
+        console.error('Error fetching classes:', error);
+        res.status(500).json({ error: 'Failed to fetch classes' });
+      }
+    });
+
+
+
+
+
+
+
+
+
+
+    app.post('/addclass',async(req,res) =>{
+        const newClasses = req.body
+        console.log(newClasses)
+        const result = await classesCollection.insertOne(newClasses)
+        res.send(result)
+        
+    })
+
+    // app.put('/coffee/:id', async(req,res) =>{
+    //   const id = req.params.id
+    //   const filter = {_id: new ObjectId(id)}
+    //   const options = {upsert: true}
+    //   const updatedCoffee = req.body;
+    //   const coffee = {
+    //     $set:{
+    //       name: updatedCoffee.name, 
+    //       quantity: updatedCoffee.quantity, 
+    //       supplier: updatedCoffee.supplier, 
+    //       taste: updatedCoffee.taste, 
+    //       category: updatedCoffee.category, 
+    //       details: updatedCoffee.details, 
+    //       photo: updatedCoffee.photo
+    //     }
+    //   }
+    //   const result = await coffeeCollection.updateOne(filter,coffee,options)
+    //   res.send(result)
+    // })
+
+    // app.delete('/coffee/:id',async(req,res) =>{
+    //   const id = req.params.id
+    //   const query = {_id: new ObjectId(id)}
+    //   const result = await coffeeCollection.deleteOne(query)
+    //   res.send(result)
+    // })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req,res) =>{
+    res.send('my another music mentor ')
+})
+
+app.listen(port,() =>{
+    console.log(`music  store running${port}`)
+})
