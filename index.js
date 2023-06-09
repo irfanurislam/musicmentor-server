39fd76d082abed026f04c5d217cdc33deef5916e
irfanurislam <mdirfanurislam@gmail.com>
2023-06-09 16:23:59 +0600

    delete card

--- index.js
+++ index.js
@@ -91,7 +91,13 @@ async function run() {
       res.send(result)
 
     })
-  
+
+    app.delete('/carts/:id',async(req,res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
 
