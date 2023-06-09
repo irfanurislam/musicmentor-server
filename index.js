6c892be27a5a77445cc5a4ec5e8d583d86365c73
irfanurislam <mdirfanurislam@gmail.com>
2023-06-09 21:27:51 +0600

    users delete one

--- index.js
+++ index.js
@@ -71,7 +71,16 @@ async function run() {
       res.send(result);
 
     })
+
+    app.delete('/users/:id',async(req,res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // role instructor
+
 
 
 
