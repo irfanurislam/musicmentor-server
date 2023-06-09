2aceed9c4e9717b170ce913a2e0abd4e1a4478a5
irfanurislam <mdirfanurislam@gmail.com>
2023-06-09 21:19:32 +0600

    role admin

--- index.js
+++ index.js
@@ -56,6 +56,22 @@ async function run() {
       res.send(result);
     })
 
+    // role admin
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+    // role instructor
 
 
 
